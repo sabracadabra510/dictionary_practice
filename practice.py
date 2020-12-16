0619ef766e3e3354a827a567464e999c3467c9fe
Sabby510 <sabrinakporter@gmail.com>
2020-12-15 19:43:31 -0800

    dictionary practice part 1

---
+++ practice.py
@@ -0,0 +1,30 @@
+# alien_0 = {'color': 'green', 'points': 5}
+
+# print(alien_0['color'])
+# print(alien_0['points'])
+
+# new_points = alien_0['points']
+
+# print(f"You have just earned {new_points} points!")
+
+# things_to_do = {'write_letters': 4, 'chores': 5, 'buy_presents': 10}
+
+# presents_to_buy = things_to_do['buy_presents']
+# print(f"You have {presents_to_buy} presents to buy before next week!")
+
+# children_in_fam = {'Porter': 3, 'Wells': 3, 'Bradley': 2, 'Espinoza': 5}
+
+# most_children = children_in_fam['Espinoza']
+
+# print(f"You have the most children out of everyone, with a total of {most_children}, wow!")
+
+favorite_foods = {
+    'Jenny':'pizza',
+    'Justin':'pasta',
+    'Dominic':'Indian food',
+    'Shannon':'steak',
+    'Dad':'chicken',
+}
+
+food = favorite_foods['Dad']
+print(f"The favorite food of Dad is {food}.")
