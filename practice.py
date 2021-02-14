a77a83e493a213f31f3b42b65111dc7c379c3c9c
Sabrina Porter <sabrinakporter@gmail.com>
2021-02-13 20:35:31 -0800

    interview cake problems

--- practice.py
+++ practice.py
@@ -448,15 +448,30 @@
 #     return True 
 
     
-def can_two_movies_fill_flight(movie_lengths, flight_length):
-    movie_lengths_seen = set()
+# def can_two_movies_fill_flight(movie_lengths, flight_length):
+#     movie_lengths_seen = set()
     
-    for first_movie_length in movie_lengths:
-        matching_second_movie_length = flight_length - first_movie_length
-        if matching_second_movie_length in movie_lengths_seen:
-            return True
+#     for first_movie_length in movie_lengths:
+#         matching_second_movie_length = flight_length - first_movie_length
+#         if matching_second_movie_length in movie_lengths_seen:
+#             return True
         
-        movie_lengths_seen.add(first_movie_length)
+#         movie_lengths_seen.add(first_movie_length)
 
 
-    return False
+#     return False
+
+def has_palindrome_permutation(the_string):
+    # Track characters we've seen an odd number of times
+    unpaired_characters = set()
+
+    for char in the_string:
+        if char in unpaired_characters:
+            unpaired_characters.remove(char)
+        else:
+            unpaired_characters.add(char)
+
+    # The string has a palindrome permutation if it
+    # has one or zero characters without a pair
+    return len(unpaired_characters) <= 1
+    
